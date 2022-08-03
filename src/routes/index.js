5fe72af9e0ea24cdb82d971dbc36c905e1e2c4e5
Loice-vanessa <icyezavanessa@gmail.com>
2022-08-03 03:43:30 +0200

    discover page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Routes,Route } from "react-router-dom";
 import HomeLayout from "../components/HomeLayout";
 import AboutUs from "../views/AboutUs"
 import Learn from "../views/learn"
+import discover from "../views/discover"
 
 
 const index=()=>{
@@ -13,6 +14,7 @@ const index=()=>{
             {/* <Route exact path="/"element={<Home/>}></Route> */}
             <Route exact path="/aboutUs"element={<AboutUs/>}></Route>
             <Route exact path="/learn"element={<Learn/>}></Route>
+            <Route exact path="/discover"element={<discover/>}></Route>
         </Routes>
         </HomeLayout>
         </>
---
+++ src/views/discover.js
@@ -0,0 +1,52 @@
+import React from "react";
+// import "./learn.css";
+
+import photo from "../assets/images/learn.jfif";
+import "antd/dist/antd.css";
+// import './index.css';
+import { Card } from "antd";
+import Footer from "../components/Footer"
+const discover = () => {
+  return (
+    <>
+      <div className="background">
+        <img src={photo} alt="image" className="backimage"></img>
+        <h1>discover with us...</h1>
+        <h3>are you in rwanda and struggling to make first sentence?Don't warry</h3>
+      </div>
+      <Card
+        style={{
+          width: 300,
+        }}
+      >
+        <p>OUR VISION</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      );
+      <Card
+        style={{
+          width: 300,
+        }}
+      >
+        <p>OUR VISION</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      );
+      <Card
+        style={{
+          width: 300,
+        }}
+      >
+        <p>OUR VISION</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      );
+      <Footer/>
+      
+    </>
+  );
+};
+export default discover;
